877dba3c21ba53241209ed7036c7d1acc8f98e7f
kuokxuen <tkxuen@gmail.com>
2022-01-09 14:06:32 +0000

    add load data fn

--- util.py
+++ util.py
@@ -1,6 +1,17 @@
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import os
+
+def load_data(path):
+    """
+    Load dataset
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data.split('\n')
 
 def tokenize(x):
     """
